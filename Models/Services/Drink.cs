826170d0b442dfc6454ef97068ece314d058555c
MariaSirb <sirbmaria27@gmail.com>
2023-03-02 21:18:29 +0200

    popup esuat

--- Models/Services/Drink.cs
+++ Models/Services/Drink.cs
@@ -1,12 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing.Drawing2D;
 
 namespace EventPlannerWebApplication.Models.Services
 {
     public class Drink
     {
         public int ID { get; set; }
-        public string DenumireBautura { get; set; }
+     
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$", ErrorMessage = "Numele Locatiei trebuie sa fie cu litera mare (ex. Briliant)!")]
+        [Display(Name = "Tipul bauturii")]
+        public string DenumireBautura { get; set; }
+       
+        [Column(TypeName = "decimal(8,2)")]
+        [Range(0.01, 500)]
+        [Display(Name = "Pret (lei)/persoana")]
         public decimal PretBautura { get; set; }
         public string BauturaImage { get; set; }
         [NotMapped]
